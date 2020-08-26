68e58e035ee835931c58bccf8d3a04f3f92f8d1d
xgqfrms <xgqfrms@outlook.com>
2020-08-26 23:29:08 +0800

    Create print.dart

---
+++ 000-xyz/dart-tutorials/print.dart
@@ -0,0 +1,33 @@
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2019-01-01
+ *
+ * @description
+ * @augments
+ * @example
+ *
+ */
+
+/* void main() {
+  dynamic d;
+  var x = "";
+  int i = 0;
+  double f = 1.0;
+  String s = "";
+  print("x = x");
+  print("i = i");
+  print("f = f");
+  print("s = s");
+  print("Hello Dart! 🎯");
+  // do something...
+  // return null;
+} */
+
+main() {
+  // print("Hello World")
+  //  Error: Expected ';' after this.
+  print("Hello World");
+}
