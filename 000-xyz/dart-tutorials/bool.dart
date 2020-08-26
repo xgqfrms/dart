e593ebaf1f357e825b3dcc2d3bf8c799685a4daf
xgqfrms <xgqfrms@outlook.com>
2020-08-26 20:28:14 +0800

    bool

---
+++ 000-xyz/dart-tutorials/bool.dart
@@ -0,0 +1,26 @@
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2019-01-01
+ *
+ * @description
+ * @augments
+ * @example
+ *
+ */
+
+void main() {
+  bool boolean = true;
+  print("boolean = ${boolean}");
+  boolean = false;
+  print("boolean = ${boolean}");
+}
+
+/*
+
+boolean = true
+boolean = false
+
+ */
