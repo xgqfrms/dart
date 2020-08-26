c1b4236df1e5cbc9598c4c072310e65d5271b321
xgqfrms <xgqfrms@outlook.com>
2020-08-26 23:35:06 +0800

    list === array

---
+++ 000-xyz/dart-tutorials/list/list.dart
@@ -0,0 +1,25 @@
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2019-01-01
+ *
+ * @description
+ * @augments
+ * @example
+ *
+ */
+
+void main() {
+  dynamic d;
+  var x = "";
+  int i = 0;
+  double f = 1.0;
+  String s = "";
+  print("x = x");
+  print("i = i");
+  print("f = f");
+  print("s = s");
+  print("Hello Dart! 🎯");
+}
