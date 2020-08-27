e3bb0fc72ba4f1072ddf70dea16254cb90a7d424
xgqfrms <xgqfrms@outlook.com>
2020-08-27 21:46:38 +0800

    forEach 自定义函数

--- 000-xyz/dart-tutorials/map/forEach.dart
+++ 000-xyz/dart-tutorials/map/forEach.dart
@@ -28,6 +28,12 @@ void main() {
   // type '(Object) => void' is not a subtype of type '(int, String) => void' of 'f'
   map.forEach((k, v) => {print("k, v = $k, $v")});
   print("\n");
+  // forEach 自定义函数
+  void for_each_callback(key, value) {
+    print("key, value = $key, $value");
+  }
+  map.forEach(for_each_callback);
+  print("\n");
   print("map.containsKey(1) = ${map.containsKey(1)}");
   print("map.containsValue('Go') = ${map.containsValue("Go")}");
   map.remove(2);
@@ -50,6 +56,10 @@ map.isNotEmpty= true
 k, v = 1, Dart
 k, v = 2, Flutter
 k, v = 3, Go
+
+key, value = 1, Dart
+key, value = 2, Flutter
+key, value = 3, Go
 
 map.containsKey(1) = true
 map.containsValue('Go') = true
